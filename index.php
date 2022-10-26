02a0c74fc17cd773d041789dbd23b5f1f6ea256f
ANDREIP211109 <81253375+ANDREIP211109@users.noreply.github.com>
2022-10-26 13:44:40 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$command=isset($_GET['command'])?$_GET['command']:'home';
+echo $command;
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+  <title>Bank</title>
+  </head>
+  <body>
+    <h1>Bank</h1>
+  </body>
+</html>
